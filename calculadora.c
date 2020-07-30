f671bc597417327bb546845d7c0c960fe48ce13a
Leo-de-Riv3r <leoguti182@gmail.com>
2020-07-30 17:43:38 -0300

    codigo mejorado, ahora puede sacar promedio y el tanto porciento de x numero

--- calculadora.c
+++ calculadora.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
+
 int main(){
-   float result, nums;
-   int v,i,x,ctrl=1;
-   int cont=0;
-  
-   do{
+int ctrl = 1;
+do{
+float result=0,nums,acum=0;
+int v,i,x;
+int cont=0;
+
    printf("\n\nCuantos numeros va a utilizar? \n");
    scanf("%d", &v);
    
    float valores[v];
    int longitud = sizeof(valores) / sizeof(valores[0]);
    
-   printf("\n\nQue operacion desea realizar?\n 0-Salir \n 1-Suma \n 2-Resta \n 3-Multiplicacion \n 4-Division \n 5-Promedio \n");
+   printf("\n\nQue operacion desea realizar?\n 0-Salir \n 1-Suma \n 2-Resta \n 3-Multiplicacion \n 4-Division \n 5-Factorial \n 6-Promedio \n 7-El tanto porciento de \n");
    scanf("%d", &i);
    
    switch(i){
@@ -21,12 +23,13 @@ int main(){
    	    break;
    	  	
       case 1: //funciona
-      for (x = 0; x < longitud; x++){
+      for (x; x < longitud; x++){
       printf("Ingrese el numero %i\n", x);
       scanf("%f", &nums);
       valores[x] = nums;
       result += valores[x];
       }
+      printf("El resultado es %f", result);
       break;
       
       case 2://funciona
@@ -36,11 +39,13 @@ int main(){
       valores[x] = nums;
       if (cont < 1){
         result = valores[0];
-	  }
-      result -= valores[x];
+	  }else{
+      result = result - valores[x];
       }
       cont++;
-      break;   
+      }
+      printf("El resultado es %f", result);
+	  break;   
 	  
 	  case 3://funciona
       for (x = 0; x < longitud; x++){
@@ -49,31 +54,65 @@ int main(){
       valores[x] = nums;
       if (cont < 1){
         result = valores[0];
-	  }
-      result *= valores[x];
+	  }else{
+      result = result * valores[x];
       }
       cont++;
-      break;    
+	  }
+	  printf("El resultado es %f", result);
+	  break;    
      
-      case 4:// no funciona
+      case 4://funciona
       for (x = 0; x < longitud; x++){
       printf("Ingrese el numero %i\n", x);
       scanf("%f", &nums);
       valores[x] = nums;
       if (cont < 1){
         result = valores[0];
-	  }
+	  }else{
       result /= valores[x];
       }
-      cont++;
-      break;   
+	  cont++;
+	  }
+	  printf("El resultado es %f", result);
+	  break;   
+      
+      case 5://funciona
+      printf("Ingrese el numero");
+      scanf("%f", &nums);
+      result = 1;
+      while (nums > 1){
+        result *= nums;
+      	nums--;
+	  }
+	  printf("El resultado es %f", result);
+	  break;  
+      
+      case 6:
+      for (x = 0; x < longitud; x++){
+        printf("Ingrese el numero %i\n", x);
+        scanf("%f", &nums);
+        acum = acum + nums;
+        cont++;
+      }
+      result = acum / cont;
+      printf("El resultado es %f", result);
+      break;
+      
+      case 7:
+      printf("Ingrese la cantidad que representa al 100 porciento:");
+      scanf("%f", &nums);
+      printf("Ingrese el porcentaje del total que desea averiguar:");
+      scanf("%f", &acum);
+      result = (acum * 100) / nums;
+      printf("El %f porciento de %f es igual a %f", acum, nums, result);
+      break;
       
       default:
       printf("Ingrese un opcion valida!");
       break;
    }
-   printf("El resultado es igual a %f", result);
-   }while(ctrl != 0);
+}while (ctrl != 0);
 
    return 0;
 }
