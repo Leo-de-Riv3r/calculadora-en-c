d139d152d3828286c417e7984a2cbeb23e9cf4b5
Leo-de-Riv3r <leoguti182@gmail.com>
2020-07-29 23:14:05 -0300

    codigo mejorado con posibilidad de sumar,restar,multiplicar,dividir y sacar promedio

--- calculadora.c
+++ calculadora.c
@@ -1,41 +1,103 @@
 #include <stdio.h>
 int main(){
-   int result;
-   int v;
-   int i;
-   int x;
-   int nums;
-   printf("Cuantos numeros va a utilizar?");//aun no funciona
-   scanf("%d", &v);
-   int valores[v];
-   int longitud = sizeof(valores) / sizeof(valores[0]);
+  float result;
+  int cont=0;
+  int v,i,x,ctrl;
+  int nums, acum;
+  do{
+  printf("Cuantos numeros va a utilizar?");
+  scanf("%d", &v);
+  int valores[v];
+  int longitud = sizeof(valores) / sizeof(valores[0]);
+  printf("Que operacion desea realizar?\n 1-Suma \n 2-Resta \n 3-Multiplicacion \n 4-Division \n 5- Promedio \n 6-Salir\n");
+  scanf("%d", &i);
 
-   printf("Que operacion desea realizar?\n 1-Suma \n 2-Resta \n");
-   scanf("%d", &i);
-
-   switch(i){
+    switch(i){
+      
       case 1:
-      for (x = 0; x < longitud; x++){
-      printf("Ingrese el numero %i\n", x);
-      scanf("%d", &nums);
-      valores[x] = nums;
-      result +=valores[x];
+        for (x = 0; x < longitud; x++){
+        printf("Ingrese el numero %i\n", x);
+        scanf("%d", &nums);
+        valores[x] = nums;
+        result +=valores[x];
       }
       break;
+
       case 2:
-        for (x = 0; x < longitud; x++){
-      printf("Ingrese el numero %i\n", x);
-      scanf("%d", &nums);
-      valores[x] = nums;
-      inicio = valores[0];
-      result -= valores[x];
-      }
+        for (x = 0; x < longitud; x++)
+        {
+          printf("Ingrese el numero %i\n", x);
+          scanf("%d", &nums);
+          valores[x] = nums;
+          if (cont < 1){
+            result = valores[0];
+		      }
+		      else{
+            result -= valores[x];
+          }
+          cont++;
+        }
         break;
-      default:
+
+      case 3:
+        for (x = 0; x < longitud; x++)
+        {
+          printf("Ingrese el numero %i\n", x);
+          scanf("%d", &nums);
+          valores[x] = nums;
+            if (cont < 1){
+            	result = valores[0];
+		        }
+		        else{
+              result *= valores[x];
+            }
+          cont++;
+        }
+        break;
+
+        case 4:
+          for (x = 0; x < longitud; x++)
+          {
+            printf("Ingrese el numero %i\n", x);
+            scanf("%d", &nums);
+            valores[x] = nums;
+              if (cont < 1){
+          	    result = valores[0];
+		          }
+		          else{
+                result /= valores[x];
+              }
+            cont++;
+        }
+        break;
+        
+        case 5:
+
+        for (x = 0; x < longitud; x++)
+        {
+          printf("Ingrese el numero %i\n", x);
+          scanf("%d", &nums);
+          valores[x] = nums;
+          acum += nums; 
+        }
+        result = acum / cont;
+        break;
+        
+        case 6:
+          printf("Chau, hasta la proxima, que tengas un buen dia!");
+          ctrl = 0;
+
+        default:
         printf("Ingrese un opcion valida!");
         break;
    }
 
-   printf("El resultado es igual a %i", result);
-   return 0;
+   printf("El resultado es igual a %f", result);
+  }
+  while(ctrl != 0);
+
+  return 0;
 }
+
+
+
